0998b9d34e059cb448c757b78f44e7b26f64bb32
daniepusb <danie.p.usb@gmail.com>
2020-08-19 01:45:53 -0300

    print level 2 and deeper levels

--- assets/js/clicks.js
+++ assets/js/clicks.js
@@ -3,25 +3,32 @@ Aqui encontraras:
     Event Listener
 */
 document.addEventListener('click', function (event) {
-    var el = event.target
+    let el = event.target
 
     if(el.matches('span')){//En caso de que se presione el span y no el boton
         el = el.parentNode
     }
+    //console.log(el);
 
     if (el.matches('#inicio') ){
-        if (el.classList.contains('seleccionado') ){
-            reiniciar()//aplicar reinicio
-        }else{
-            el.classList.add('seleccionado')//marcar opcion seleccionada
+        if (!el.classList.contains('seleccionado') ){
+            el.classList.add('seleccionado')        //marcar seleccion
             iniciar()//leer primer contenido
+        }else{
+            el.classList.remove('seleccionado')     //desmarcar seleccion
+            reiniciar_resultado()                   //reinicio en resultado
+            reiniciar_tope(el)
         }
     }else if (el.matches('.btn') ){
-        el.classList.add('seleccionado') //marcar opcion seleccionada
+        el.classList.add('seleccionado')            //marcar seleccion
+        document.getElementById('tope').appendChild(el)//apend opcion a tope con inicio
+        reiniciar_resultado()                       //reinicio en resultado
         if( el.dataset.file ){
-            obtener_archivo( el.dataset.file )
+            obtener_archivo( el.dataset.file )//fetch file del seleccionado
+        }else if( el.dataset.content ){
+            imprimir_contenido(  JSON.parse( el.dataset.content )   )
         }else{
-            return
+            //has llegado a una especializacion
         }
     } else {
         return
--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,20 @@ Here yo uwill find:
 let _inicio
 let _json_archivo
 
-
 /* Funcion asincorona para obtener un contenido inicial par mostrar */
 async function iniciar(){
-    const response      = await fetch('assets/data/contenido.json')
+    let response        = await fetch('assets/data/contenido.json')
     _inicio             = await response.json()
 
     imprimir_contenido(_inicio.content)
-    //dibujar_en_canvas()
-    //console.log(_inicio)
 }
 
 /* Funcion asincorona para obtener la informacion de un archivo json */
 async function obtener_archivo(archivo){
-    const response      = await fetch("assets/data/"+archivo)
-    _json_file          = await response.json()
-    //console.log(_json_file);
+    const response      = await fetch('assets/data/'+archivo)
+    _json_archivo       = await response.json()
+    
+    imprimir_contenido(_json_archivo)
 }
 
 /* Funcion para desglosar el contenido del json | parametro array */
@@ -33,7 +31,7 @@ function imprimir_contenido(arreglo){
     for (x in arreglo) {
         let boton = obtener_boton(arreglo[x])
         botones += boton
-        console.log(arreglo[x])
+        //console.log(arreglo[x])
     }
 
     document.getElementById('resultado').innerHTML = botones
@@ -44,22 +42,37 @@ function imprimir_contenido(arreglo){
 
 /* Funcion regresa el template html para un nodo */
 function obtener_boton(parametro){
-    const boton = `<div class="btn" data-name="${parametro.name}" data-hash="${parametro.hash}" data-level="${parametro.level}" data-content="${parametro.content}" data-file="${parametro.file}" ><span class="nombre">${parametro.name}</span></div>`
+    let pcontenido = JSON.stringify( parametro.content )
+
+    const boton = `<div class="btn" 
+    data-name="${parametro.name}"
+    data-hash="${parametro.hash}" 
+    data-level="${parametro.level}" 
+    data-content='${pcontenido}' 
+    data-file="${parametro.file}" >
+    <span class="nombre">${parametro.name}</span>
+    </div>`
+
     return boton
 }
 
-/* Funcion regresa reinicia todo de nuevo borrando del DOM todos los hijos de #inicio */
-function reiniciar(){
+/* Funcion borra del DOM todos los hijos de #resultado */
+function reiniciar_resultado(){
     document.getElementById('resultado').innerHTML = ""
-    document.getElementById('inicio').classList.remove('seleccionado')
+}
+
+/* Funcion borra del DOM todo el contenido de #inicio y le coloca el primer botono #inicio nuevamente pasado por parametro*/
+function reiniciar_tope(el){
+    tope = document.getElementById('tope')
+    tope.innerHTML = ""
+    tope.appendChild(el)
 }
 
 
 /**
-    //obtener_inicio
-    //leer content
+    //obtener_inicio*
+    //leer content*
     //print next level
-    //draw html
     //put animation
     //create suggestions
     //add    suggestions
